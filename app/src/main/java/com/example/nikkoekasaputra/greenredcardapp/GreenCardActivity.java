8991c7391d1dbf22d2492cba8a0c6d53f80ebd63
NikkoES <nikkoeka04@gmail.com>
2017-11-18 23:49:42 +0700

    add more layout (it's not flow)

---
+++ app/src/main/java/com/example/nikkoekasaputra/greenredcardapp/GreenCardActivity.java
@@ -0,0 +1,13 @@
+package com.example.nikkoekasaputra.greenredcardapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class GreenCardActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_green_card);
+    }
+}
